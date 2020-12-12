dd5cd0855dc3cedb6c435efa9d55dd07b1063d88
Ferry Kurniawan <ferryku8@gmail.com>
2020-12-12 14:21:10 +0700

    TUGAS 4 C++
    Mengumpulkan tugas 4 C++

---
+++ Tugas 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x =30;
+	cout<<"Nilai Awal x = "<<x<<endl;
+	x--;
+	cout<<"Nilai Kedua x = "<<x<<endl;
+	x++ , x++;
+	cout<<"Nilai Ketiga x = "<<x;
+}
